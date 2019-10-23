929ac3e6808eb96fe5454c7cf6059f56bbd260e7
Samuel Caires Ribeiro <samuelribeiro159@outlook.com>
2019-10-23 16:56:55 +0100

    Inital set up on views and controllers

--- Sparta_Online_Shop/Sparta_Online_Shop/Controllers/AdminController.cs
+++ Sparta_Online_Shop/Sparta_Online_Shop/Controllers/AdminController.cs
@@ -8,8 +8,36 @@ namespace Sparta_Online_Shop.Controllers
 {
     public class AdminController : Controller
     {
+        private int? totalSales;
         // GET: Admin
         public ActionResult Index()
+        {
+            using (var db = new SpartaShopModel())
+            {
+                var orderDetails = new List<OrderDetail>();
+                orderDetails = db.OrderDetails.ToList();
+
+                foreach(OrderDetail od in orderDetails)
+                {
+                    totalSales += od.Quantity;
+                }
+            }
+
+            ViewBag.TotalSales = totalSales;
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult StockOrders()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
         {
             return View();
         }
