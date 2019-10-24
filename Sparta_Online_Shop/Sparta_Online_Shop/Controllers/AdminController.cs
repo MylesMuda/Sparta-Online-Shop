7a99b760ec5e7d6c5ed31bb7ba675c4416eb3a00
Sam <samuel.c.ribeiro@outlook.com>
2019-10-24 19:43:31 +0100

    Added profit and sales values to overview page

--- Sparta_Online_Shop/Sparta_Online_Shop/Controllers/AdminController.cs
+++ Sparta_Online_Shop/Sparta_Online_Shop/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,8 +9,11 @@ namespace Sparta_Online_Shop.Controllers
 {
     public class AdminController : Controller
     {
-        private int? totalSales;
-        // GET: Admin
+        private int? totalSales = 0;
+        private decimal? Profit = 0;
+        private decimal? ProfitDay= 0;
+        private decimal? ProfitMonth = 0;
+        private decimal? ProfitYear = 0;
         public ActionResult Index()
         {
             using (var db = new SpartaShopModel())
@@ -17,13 +21,43 @@ namespace Sparta_Online_Shop.Controllers
                 var orderDetails = new List<OrderDetail>();
                 orderDetails = db.OrderDetails.ToList();
 
-                foreach(OrderDetail od in orderDetails)
+                foreach (OrderDetail od in orderDetails)
                 {
-                    totalSales += od.Quantity;
+                    totalSales += od.Quantity;              //Totals the quanity of items sold
                 }
+
+                var orders = new List<Order>();
+
+                orders = db.Orders.OrderByDescending(o => o.OrderDate).ToList();
+
+                foreach (var order in orders)
+                {
+                    Profit += order.TotalCost;              //Totals all income
+
+                    if (order.OrderDate > DateTime.Now.AddDays(-1))
+                    {
+                        ProfitDay += order.TotalCost;       //Totals income from last day
+                    }
+
+                    if (order.OrderDate > DateTime.Now.AddMonths(-1))
+                    {
+                        ProfitMonth += order.TotalCost;     //Totals income from last month
+                    }
+
+                    if (order.OrderDate > DateTime.Now.AddYears(-1))
+                    {
+                        ProfitYear += order.TotalCost;      //Totals income from last year
+                    }
+                }
+
             }
 
             ViewBag.TotalSales = totalSales;
+            ViewBag.Profit = Profit;
+            ViewBag.ProfitDay = ProfitDay;
+            ViewBag.ProfitMonth = ProfitMonth;
+            ViewBag.ProfitYear = ProfitYear;
+
             return View();
         }
 
