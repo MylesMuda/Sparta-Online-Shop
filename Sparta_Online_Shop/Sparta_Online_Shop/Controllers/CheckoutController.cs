6b1626decfd3d5568daf9492478e0bceceeb29fd
Fuat <robo.ai@outlook.com>
2019-10-29 09:36:32 +0000

    Added checkout views

--- Sparta_Online_Shop/Sparta_Online_Shop/Controllers/CheckoutController.cs
+++ Sparta_Online_Shop/Sparta_Online_Shop/Controllers/CheckoutController.cs
@@ -13,9 +13,45 @@ namespace Sparta_Online_Shop.Controllers
         {
             return View();
         }
+
         public ActionResult Basket()
         {
             return View();
+        }
+
+        public ActionResult CheckoutError()
+        {
+            return View();
+        }
+
+        public ActionResult CheckoutSuccessful()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BasketPost(string Amount)
+        {
+            float price;
+            ViewBag.Message = Amount;
+
+            if (float.TryParse(Amount, out price))
+            {
+                ViewBag.Message = Amount;
+                return View("Checkout");
+            }
+
+            return View("CheckoutSuccessful");
+        }
+
+        [HttpPost]
+        public ActionResult PaypalPost(string orderID)
+        {
+            //clear basket if successful
+
+            ViewBag.Message = orderID;
+
+            return RedirectToAction("CheckoutSuccessful");
         }
     }
 }
